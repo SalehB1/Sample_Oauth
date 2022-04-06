2821006469334b545ab414d4dffb7337ba6f9dfc
Max <salehbahar90@gmail.com>
2022-04-06 14:27:37 +0430

    adding home

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path('seller_logout/', SellerLogout.as_view(), name='Logout'),
     # path('seller_register/', SellerRegister.as_view(), name='Register'),
     # path('dashboard/',dashboard.,name='dashboard')
+    path('user/login/',view)
 ]
